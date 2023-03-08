56f6bdc8ea3e17b080d745763babdb1837f4242e
bonalasaiprakash <107921112+bonalasaiprakash@users.noreply.github.com>
2023-03-08 12:40:33 +0000

    Update ec2.tf

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,15 +1,15 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA5SZF4BTGRJ5PJUPD"
-  secret_key = "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"
+  access_key = "KIA35MGAHN6EQUB5CM5"
+  secret_key = "N+IlcIDSyyKtK6uY8dqcFL7lKs5XLEpQ1RBAq5XB"
   }
 
   resource "aws_instance" "server" {
   ami           = "ami-0a6b2839d44d781b2"
   instance_type = "t2.micro"
   subnet_id     = "subnet-0c9cb770dfd5a16ab"
-  security_groups = ["sg-0869c6a165f2f8e36"]
-  key_name      = "prakash"
+  security_groups = ["sg-08a490e0b81430ca5"]
+  key_name      = "dog"
 
   tags = {
     Name = "sai"
